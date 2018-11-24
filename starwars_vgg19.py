ce428633e047d483856d8bbf025f9d0dcc94b5f1
Kevin Jacobson <keebinie@mailbolt.com>
2018-11-23 18:00:20 -0800

    Import of classes seems to work
    todo: the fit function is complaining that the numpy arrays are not the
    right size.

--- starwars_vgg19.py
+++ starwars_vgg19.py
@@ -4,18 +4,51 @@ Star Wars VGG19
 
 """
 
-from keras_preprocessing import image as image_utils
-from keras_applications.imagenet_utils import decode_predictions
-from keras_applications.imagenet_utils import preprocess_input
-from keras_applications import vgg19
-from keras_applications import resnet50
+from keras import backend as K
+
+from keras.preprocessing import image as image_utils
+from keras.applications.imagenet_utils import decode_predictions
+from keras.applications.imagenet_utils import preprocess_input
+from keras.applications import vgg19
 import numpy as np
 #import argparse
 import cv2
+import os
 
 #orig = cv2.imread('./TestData/ro9kcvX.jpg')
 
-image = image_utils.load_img('./TestData/ro9kcvX.jpg', target_size=(224,224))
+classpath = './Classes/'
 
-model = vgg19.VGG19(include_top=True,input_shape=(224, 224, 3),weights=None,classes=24)
+test_images = []
+training_images = []
+training_classes = []
 
+print("Loading training images...")      
+#load training images 
+for dirname, dirnames, filenames in os.walk(classpath):
+
+    for filename in filenames:
+        #load an image and prepare it
+        image = image_utils.load_img(os.path.join(dirname, filename), target_size=(224,224))
+        image = image_utils.img_to_array(image)
+        #image = np.expand_dims(image, axis=0)
+        image = preprocess_input(image)
+        training_images.append(image)
+        #class name associated with a particular image
+        training_classes.append(str(dirname.split('/')[-1:][0]))
+
+#training_images = np.array(training_images)
+#training_classes = np.array(training_classes)
+"""
+print(str(training_images))
+print(str(training_images.shape))
+input()
+print(str(training_classes))
+print(str(training_classes.shape))
+input()
+""" 
+model = vgg19.VGG19(include_top=True,weights=None,classes=24)
+
+model.compile(loss='mean_squared_error', optimizer='sgd')
+
+model.fit(x=training_images, y=training_classes)
